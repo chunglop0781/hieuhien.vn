6d3245dfdcf752bd0a94216161589793f3af77da
Hieuhien.vn <hotro.anhvustore@gmail.com>
2018-10-05 08:56:03 +0700

    Update HieuHienVNPlaylist.py

--- HieuHienVNPlaylist.py
+++ HieuHienVNPlaylist.py
@@ -1053,8 +1053,8 @@ def LoginFShare(uname,pword):
 def GetFShareCred():
 	try:
 		_hash = plugin.get_setting("hash")
-		uname = "vehaipro@gmail.com"
-		pword = "khoahoai"
+		uname = plugin.get_setting("Ufacebook")
+		pword = plugin.get_setting("Upassword")
 		if _hash != (uname+pword): 
 			plugin.set_setting("cred","")
 		cred  = json.loads(plugin.get_setting("cred"))
@@ -1063,8 +1063,8 @@ def GetFShareCred():
 		return cred
 	except:
 		try:
-			uname = "vehaipro@gmail.com"
-			pword = "khoahoai"
+			uname = plugin.get_setting("Ufacebook")
+			pword = plugin.get_setting("Upassword")
 			cred = LoginFShare(uname,pword)
 			user = GetFShareUser(cred)
 			LoginOKNoti(user["email"], user["level"])
